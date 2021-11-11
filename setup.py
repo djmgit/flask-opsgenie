168a86493dcf426a39fd5b4d1315823e4cc0b2b5
djmatlas <djmdeveloper060796@gmail.com>
2021-11-11 23:20:27 +0530

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""
+Flask-Opsgenie
+-------------
+
+Flask extension for opsgenie
+"""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="Flask-Opsgenie",
+    url="",
+    download_url="",
+    license="",
+    author="",
+    author_email="",
+    description="",
+    long_description=__doc__,
+    zip_safe=False,
+    platforms="any",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+    ],
+    setup_requires=[""],
+    vcversioner={},
+)
