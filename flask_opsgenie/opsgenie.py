4db36823293db1722255e7cd8b9c26d5d2b0dc5f
djmatlas <djmdeveloper060796@gmail.com>
2021-11-18 22:53:53 +0530

    Fix typo

--- flask_opsgenie/opsgenie.py
+++ flask_opsgenie/opsgenie.py
@@ -2,6 +2,8 @@ from typing import Optional
 from flask import request
 from flask_opsgenie.entities import AlertType
 
-def raise_opsgenie_alert(alert_type:AlertType, alert_status_code:Optional[int], \
-        alert_status_class:Optional[str], elapsed_time:Optional[int]) -> None:
-    pass
+def raise_opsgenie_alert(alert_type:AlertType = None, alert_status_code:Optional[int] = None, \
+        alert_status_class:Optional[str] = None, elapsed_time:Optional[int] = None) -> None:
+
+    endpoint = request.url_rule
+    method = request.method
