09c4ffa3339723cedfe4a04ee1793e4336408dbe
djmatlas <djmdeveloper060796@gmail.com>
2021-11-29 23:07:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,42 @@
 """
 Flask-Opsgenie
--------------
+---------------
 
-Flask extension for opsgenie
+An easy to use Opsgenie extension for flask. Allows user
+to raise an opsgenie alert on unwanted response status code,
+increased response latency and on unhandled exception thrown
+by routes.
+With flask-opsgenie, you will no more have to add alerting
+logic to your code manually, rather all you need to do is configure
+this extension with different alert conditions and attributes.
 """
 
 from setuptools import find_packages, setup
 
+def get_dependencies():
+  with open("requirements.txt") as req:
+    lines = [line.strip() for line in req.readlines()]
+    return lines
+
 setup(
     name="Flask-Opsgenie",
-    url="",
+    url="https://github.com/djmgit/flask-opsgenie",
     download_url="",
     license="",
-    author="",
+    author="Deepjyoti Mondal",
     author_email="",
-    description="",
+    description="Opsgenie extension for Flask",
     long_description=__doc__,
     zip_safe=False,
     platforms="any",
     packages=find_packages(),
-    install_requires=[
-    ],
+    install_requires=get_dependencies(),
     classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        #'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
-    setup_requires=[""],
     vcversioner={},
 )
