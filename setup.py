5eacf26acd1882981932d731c50c2410b2efcda8
djmatlas <djmdeveloper060796@gmail.com>
2021-12-09 17:28:48 +0530

    get rid of requirements.txt

--- setup.py
+++ setup.py
@@ -13,24 +13,22 @@ this extension with different alert conditions and attributes.
 
 from setuptools import find_packages, setup
 
-def get_dependencies():
-  with open("requirements.txt") as req:
-    lines = [line.strip() for line in req.readlines()]
-    return lines
-
 setup(
     name="Flask-Opsgenie",
     url="https://github.com/djmgit/flask-opsgenie",
     license="",
     author="Deepjyoti Mondal",
     description="Opsgenie extension for Flask",
-    download_url="https://github.com/djmgit/flask-opsgenie/archive/refs/tags/v0.0.6.tar.gz",
+    download_url="https://github.com/djmgit/flask-opsgenie/archive/refs/tags/v0.0.7.tar.gz",
     long_description=__doc__,
     zip_safe=False,
     keywords = ['Alerting', 'flask', 'web', 'Reliability', 'DevOps'],
     platforms="any",
     packages=find_packages(),
-    install_requires=get_dependencies(),
+    install_requires=[
+        "requests>=2.0.0",
+        "Flask>=1.1.2"
+    ]
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -38,5 +36,5 @@ setup(
         'Programming Language :: Python',
         'Topic :: System :: Monitoring',
     ],
-    version='0.0.6'
+    version='0.0.7'
 )
